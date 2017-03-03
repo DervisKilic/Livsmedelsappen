b3bbd93da8fa07d96e837f262f6e51029569e7a6
Dervis <dervis85@gmail.com>
2017-03-03 13:09:14 +0100

    asdasd

--- MatAppen/MainTableViewController.swift
+++ MatAppen/MainTableViewController.swift
@@ -41,7 +41,7 @@ class MainTableViewController: UITableViewController, UISearchResultsUpdating {
         searchController.dimsBackgroundDuringPresentation = false
         tableView.tableHeaderView = searchController.searchBar
         searchController.searchBar.barTintColor = UIColor(red: 0.29, green: 0.384, blue: 0.278, alpha: 1.0)
-        searchController.searchBar.tintColor = UIColor(red: 0.71, green: 0.54, blue: 0.54, alpha: 1.0)
+        searchController.searchBar.tintColor = UIColor.white
         }
         tableView.backgroundColor = UIColor(red: 0.29, green: 0.384, blue: 0.278, alpha: 1.0)
         
